7e35718887416b5e328b929a20f3b2ea00926d46
Kaamouchi-Fadi <Kaamouchi.fadi@gmail.com>
2023-04-24 13:14:26 -0700

    sda git commit clean s ad
    C - printf
    
    sa
    
    wq
    sda:!
    :wq
    
     df:msg;ldfp;g:
    d*gd
    f*g*df
    g*df
    g*
    dfg*
    
    sd
    
    [3~
    
    s ads
    *
    wxd
    fegegefh
    lio
    mùo
    pù
    p^*
    *
    _oç*ç_è
    o*ik
    u-èyh*gz
    e r*&
    **
    a.out functions1.c functions2.c functions.c get_flags.c get_precision.c get_size.c get_width.c .git handle_print.c main.c main.h _printf.c README.md utils.c write_handlers.c
    &*
    
    git commit -m "
    
    sda
    git
    commit
    clean
    s ad
    
    C - printf
    
    sa
    
    wq
    sda:!
    :wq
    
     df:msg;ldfp;g:
    d$f*gd
    f*g*df
    g*df
    g*
    dfg*
    
    sd
    
    [3~
    
    s ads
    *
    wxd
    fegegefh
    lio
    mùo
    pù
    p^*
    *
    _oç*ç_è
    o*ik
    u-èyh*gz
    e r*&
    **
    a.out functions1.c functions2.c functions.c get_flags.c get_precision.c get_size.c get_width.c .git handle_print.c main.c main.h _printf.c README.md utils.c write_handlers.c
    &"*

--- functions.c
+++ functions.c
@@ -108,6 +108,7 @@ int print_percent(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
+
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
