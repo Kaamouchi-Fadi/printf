a5d0478c1434a1c46b6f4bfaf460008b66b4049a
Kaamouchi-Fadi <125515402+Kaamouchi-Fadi@users.noreply.github.com>
2023-04-25 21:00:32 +0000

    C - printf

--- get_precision.c
+++ get_precision.c
@@ -1,41 +1,39 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
- */
-int get_precision(const char *format, int *i, va_list list)
+ * get_precision - this function Extracts the precision
+ * value from a format string.
+ * @s: The format string to parse.
+ * @params: A struct containing formatting parameters for the string.
+ * @ap: A pointer to a variable argument list containing arguments to format.
+ * Return: A pointer to the next character in the
+ * format string after the precision specifier.
+*/
+
+char *get_precision(char *s, prm_t *params, va_list ap)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+	int precision = 0;
 
-	if (format[curr_i] != '.')
-		return (precision);
-
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	/* Check if the format string contains a precision specifier */
+	if (*s != '.')
+		return (s);
+	s++;
+	if (*s == '*')
 	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
+		precision  = va_arg(ap, int);
+		s++;
 	}
-
-	*i = curr_i - 1;
-
-	return precision;
+	/**
+	 * If the next character is not '*', loop through the following
+	 * characters in the format string and convert them into an integer value.
+	 */
+	else
+	{
+		for (; _isdigit(*s); s++)
+		{
+			precision = precision * 10 + (*s - '0');
+		}
+	}
+	(*params).precisions = precision;
+	return (s);
 }
